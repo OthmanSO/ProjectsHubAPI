38b119ca87d60bab7e8e29e1957585b7416edb4b
OthmanSO <othman.subhi.123@gmail.com>
2022-10-05 20:14:01 +0300

    Create the API project And .Data project

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectHubAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/V1.0/usres")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("/{id}")]
+        public ActionResult<object> GetUser(Guid id)
+        {
+            return null;
+        }
+    }
+}
