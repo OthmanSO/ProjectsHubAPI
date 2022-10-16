d49c0738e4b8596edf36c70a343a055419924263
OthmanSO <othman.subhi.123@gmail.com>
2022-10-16 04:07:09 +0300

    Add UserAccountCreate dto

---
+++ ProjectsHub.Model/UserAccountCreate.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectsHub.Model
+{
+    public class UserAccountCreate
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }    
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public Guid ProfilePicture { get; set; }
+    }
+}
