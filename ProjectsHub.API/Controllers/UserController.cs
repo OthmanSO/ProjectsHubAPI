0680ea987090d8d8d9c15c6782337ad06f0cbcc9
OthmanSO <othman.subhi.123@gmail.com>
2022-11-18 22:26:45 +0200

    Put contacts

--- ProjectsHub.API/Controllers/UserController.cs
+++ ProjectsHub.API/Controllers/UserController.cs
@@ -252,7 +252,6 @@ namespace ProjectsHub.API.Controllers
             try
             {
                 var Contacts = _UserService.GetUserContacts(userId, _UserRepository);
-<<<<<<< HEAD
                 List<IdDto> ContactsList = new List<IdDto>();
                 foreach (var Contact in Contacts)
                 {
@@ -268,15 +267,6 @@ namespace ProjectsHub.API.Controllers
             {
                 return NotFound("user Not Found");
             }
-=======
-                return Ok(Contacts);
-            }
-            catch(ArgumentNullException e)
-            {
-                return NotFound("user Not Found");
-            }
-            
->>>>>>> 3536ffd (fixed the controller in get contacts)
         }
 
 
