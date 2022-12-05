f0b418194047920f3bbad704abfe31265208e5a3
OthmanSO <othman.subhi.123@gmail.com>
2022-12-05 20:36:03 +0200

    Fix recieving id in followers and following

--- ProjectsHub.API/Controllers/UserController.cs
+++ ProjectsHub.API/Controllers/UserController.cs
@@ -240,6 +240,67 @@ namespace ProjectsHub.API.Controllers
                 return NotFound("user Not Found");
             }
         }
+        [Authorize]
+        [HttpGet("Followers/{userId}")]
+        public async Task<IActionResult> GetUserFollowers(string userId)
+        {
+            Guid id;
+            try
+            {
+                if (!userId.IsNullOrEmpty())
+                {
+                    id = Guid.Parse(userId);
+                }
+                else
+                {
+                    id = _userToken.GetUserIdFromToken();
+                }
+            }
+            catch (FormatException e)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var listOfUsersFollowingUserAccount = _UserService.GetListOfFollwers(id);
+                return Ok(listOfUsersFollowingUserAccount);
+            }
+            catch (Exception e)
+            {
+                return NotFound("user Not Found");
+            }
+        }
+
+        [Authorize]
+        [HttpGet("Following/{userId}")]
+        public async Task<IActionResult> GetUserFollowing(string userId)
+        {
+            Guid id;
+            try
+            {
+                if (!userId.IsNullOrEmpty())
+                {
+                    id = Guid.Parse(userId);
+                }
+                else
+                {
+                    id = _userToken.GetUserIdFromToken();
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var listOfUsersThatUserAccountFollow = _UserService.GetListOfFollwing(id);
+                return Ok(listOfUsersThatUserAccountFollow);
+            }
+            catch (Exception e)
+            {
+                return NotFound("user Not Found");
+            }
+        }
 
         [Authorize]
         [HttpGet()]
@@ -347,68 +408,6 @@ namespace ProjectsHub.API.Controllers
                 return NotFound("user Not Found");
             }
             return Ok();
-        }
-
-        [Authorize]
-        [HttpGet("Followers/{userId}")]
-        public async Task<IActionResult> GetUserFollowers(string userId)
-        {
-            Guid id;
-            try
-            {
-                if (!userId.IsNullOrEmpty())
-                {
-                    id = Guid.Parse(userId);
-                }
-                else
-                {
-                    id = _userToken.GetUserIdFromToken();
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest();
-            }
-            try
-            {
-                var listOfUsersFollowingUserAccount = _UserService.GetListOfFollwers(id);
-                return Ok(listOfUsersFollowingUserAccount);
-            }
-            catch (Exception e)
-            {
-                return NotFound("user Not Found");
-            }
-        }
-
-        [Authorize]
-        [HttpGet("Following/{userId}")]
-        public async Task<IActionResult> GetUserFollowing(string userId)
-        {
-            Guid id;
-            try
-            {
-                if (!userId.IsNullOrEmpty())
-                {
-                    id = Guid.Parse(userId);
-                }
-                else
-                {
-                    id = _userToken.GetUserIdFromToken();
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest();
-            }
-            try
-            {
-                var listOfUsersThatUserAccountFollow = _UserService.GetListOfFollwing(id);
-                return Ok(listOfUsersThatUserAccountFollow);
-            }
-            catch (Exception e)
-            {
-                return NotFound("user Not Found");
-            }
         }
     }
 }
