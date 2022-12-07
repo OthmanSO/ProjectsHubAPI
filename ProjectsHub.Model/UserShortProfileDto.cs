83f79085dbb1dbe738a46a25482499eeed33fdb5
OthmanSO <othman.subhi.123@gmail.com>
2022-12-07 20:46:36 +0200

    Rebasing

--- ProjectsHub.Model/UserShortProfileDto.cs
+++ ProjectsHub.Model/UserShortProfileDto.cs
@@ -5,6 +5,7 @@ namespace ProjectsHub.Model
     {
         public Guid _id { get; set; }
 <<<<<<< HEAD
+<<<<<<< HEAD
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ProfilePic { get; set; }
@@ -12,5 +13,10 @@ namespace ProjectsHub.Model
         public string UserName { get; set; }
         public string ChangeProfilePic { get; set; }
 >>>>>>> 369edee (Get UserShortProfile)
+=======
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePic { get; set; }
+>>>>>>> 1d0bd47 (change short profile name to first name and lastname)
     }
 }
