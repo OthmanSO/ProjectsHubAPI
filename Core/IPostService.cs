1965cccb8f124757e4185d374fb4ca2da7303863
OthmanSO <othman.subhi.123@gmail.com>
2023-01-13 19:01:46 +0200

    Put post\like and Put post\unlike

--- Core/IPostService.cs
+++ Core/IPostService.cs
@@ -5,7 +5,9 @@ namespace ProjectsHub.Core
     public interface IPostService
     {
         public Task<PostReturnDto> CreatePost(CreatePostDto post, string userId);
-        public Task<PostReturnDto> GetPost(string id);
+        public Task<PostReturnDto> GetPost(string userid, string postId);
         public Task DeletePost(string PostId, string userId);
+        public Task LikePost(string userId, string postId);
+        public Task UnLikePost(string userId, string postId);
     }
 }
