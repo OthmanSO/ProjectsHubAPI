01c2c2e9051aab300fefd8c4d5ec29f60f4220e3
OthmanSO <othman.subhi.123@gmail.com>
2023-01-21 22:44:51 +0200

    Get UserNetwork

---
+++ ProjectsHub.Model/UserNetworkProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectsHub.Model
+{
+    public class UserNetworkProfile
+    {
+        public string _id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePic { get; set; }
+        public bool IsFollowed { get; set; }
+        public string Bio { get; set; }
+    }
+}
